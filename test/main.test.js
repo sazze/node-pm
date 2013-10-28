0246ceff07ccf50f32cee603d2f3030659ccd2df
Craig Thayer <cthayer@sazze.com>
2013-10-28 15:15:16 -0700

    more modular and more tests

--- test/main.test.js
+++ test/main.test.js
@@ -3,18 +3,63 @@ var expect = require('chai').expect;
 describe('Starting up Worker Manager', function() {
   "use strict";
   var pm = null;
-  var master = null;
 
   beforeEach(function() {
-    pm = require('../lib/main');
+    pm = require('..');
+  });
+
+  it('should require a start script', function() {
+    expect(pm.start.bind(pm.start, {s: true})).to.throw('app start script must be specified');
+  });
+
+  it('should require an existing start script', function() {
+    expect(pm.start.bind(pm.start, {s: true, _: ['fake.js']})).to.throw('cannot file application start script: fake.js');
+  });
+
+  it('should set default timeouts', function() {
+    var config = require('../lib/config');
+    expect(config).to.have.property('timeouts');
+    expect(config.timeouts).to.have.property('start').and.to.equal(30000);
+    expect(config.timeouts).to.have.property('maxAge').and.to.equal(1800000);
   });
 
   it('should spawn 4 workers', function() {
-    master = pm.start({n: 4, _: ['./test/scripts/httpServer.js']});
+    var master = pm.start({n: 4, _: ['./test/scripts/httpServer.js']});
     expect(master.count).to.equal(4);
   });
 
+  /*it('should call exit when process is killed hard', function(done) {
+    master = pm.start();
+    var workerId;
+
+    master.cluster.on('exit', function(worker) {
+      expect(worker.id).to.equal(workerId);
+      expect(worker.suicide).to.equal(true);
+      done();
+    });
+
+    for (var id in master.cluster.workers) {
+      workerId = parseInt(id);
+      process.kill(master.cluster.workers[id].process.pid, 'SIGKILL');
+      break;
+    }
+  });*/
+
+  it('should listen on more than one port', function(done) {
+    var listenCount = 0;
+    var master = pm.start({n: 1, _: ['./test/scripts/multipleHttpServers.js']});
+
+    master.cluster.on('listening', function(worker, address) {
+      listenCount++;
+
+      if (listenCount == 2) {
+        done();
+      }
+    });
+  });
+
   afterEach(function() {
-    master.exit();
+    pm.stop();
+    pm = null;
   });
 });
---
+++ test/scripts/multipleHttpServers.js
@@ -0,0 +1,5 @@
+var http = require('http').createServer();
+http.listen(58085);
+
+var http2 = require('http').createServer();
+http2.listen(58086);
