7aa5f8bd33025b4d806d705e89c610a6048a3488
Craig Thayer <cthayer@sazze.com>
2014-09-11 11:45:29 -0700

    enable mechanism for inter-process communication between worker processes and the master process

--- test/scripts/httpServer.js
+++ test/scripts/httpServer.js
@@ -1,1 +1,7 @@
 require('http').createServer().listen(0);
+
+process.on('message', function (message, handle) {
+  console.log(JSON.stringify({pid: process.pid, message: message}));
+});
+
+process.send('Hi, I\'m worker ' + process.pid);
